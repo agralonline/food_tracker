910b40959c88582db93f0ffc0547f0be6a3cb640
agralonline <agralonline@gmail.com>
2025-07-30 01:48:04 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,22 @@
-const CACHE_NAME = 'job-hours-tracker-v1';
-const urlsToCache = [
-  '.',
-  'index.html',
-  'style.css',
-  'app.js',
-  'manifest.json',
-  'icon-192.png',
-  'icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('food-tracker-cache').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.js',
+        '/manifest.json',
+        '/192.png',   // ← fixed filename
+        '/512.png'    // ← fixed filename
+      ]);
     })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if(key !== CACHE_NAME){
-          return caches.delete(key);
-        }
-      })
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => response || fetch(event.request))
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
   );
 });
